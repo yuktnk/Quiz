9f9eadba2ffbe7121dd06877e118c6c76ebdf658
yuktnk <yuki0wl@gmail.com>
2020-10-25 18:32:38 +0900

    何問正解したかを表示

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const quizzes = [
 
 const quizLength = quizzes.length;
 let quizIndex = 0;
-
+let score = 0;
 
 
 const $button = document.getElementsByTagName('button');
@@ -52,6 +52,7 @@ setupQuiz();
 const clickHandler = (e) => {
   if (quizzes[quizIndex].correct === e.target.textContent) {
     window.alert('正解！');
+    score++;
   } else {
     window.alert('不正解！');
   }
@@ -59,7 +60,7 @@ const clickHandler = (e) => {
   if (quizIndex < quizLength) {
     setupQuiz();
   } else {
-    window.alert('終了！');
+    window.alert('終了！あなたの成果数は' + score + '/' + quizLength + 'です！');
   }
 }
 
