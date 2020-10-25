d9fa04b1e59423935d31073b436de00148607f43
yuktnk <yuki0wl@gmail.com>
2020-10-25 18:19:48 +0900

    リファクタリング

--- app.js
+++ app.js
@@ -6,39 +6,31 @@ const ansewers = [
   '不正解'
 ];
 const correct = '正解';
-
 const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
 
-$button[0].textContent = ansewers[0];
-$button[1].textContent = ansewers[1];
-$button[2].textContent = ansewers[2];
-$button[3].textContent = ansewers[3];
-  
-$button[0].addEventListener('click', () => {
-  if (correct === $button[0].textContent) {
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = question;
+  let buttonIndex = 0;
+  while (buttonIndex < buttonLength) {
+    $button[buttonIndex].textContent = ansewers[buttonIndex];
+    buttonIndex++;
+  }
+}
+setupQuiz();
+
+const clickHandler = (e) => {
+  if (correct === e.target.textContent) {
     window.alert('正解！');
   } else {
     window.alert('不正解！');
   }
-});
-$button[1].addEventListener('click', () => {
-  if (correct === $button[1].textContent) {
-    window.alert('正解！');
-  } else {
-    window.alert('不正解！');
-  }
-});
-$button[2].addEventListener('click', () => {
-  if (correct === $button[2].textContent) {
-    window.alert('正解！');
-  } else {
-    window.alert('不正解！');
-  }
-});
-$button[3].addEventListener('click', () => {
-  if (correct === $button[3].textContent) {
-    window.alert('正解！');
-  } else {
-    window.alert('不正解！');
-  }
-});
+}
+
+let handleIndex = 0;
+while (handleIndex < buttonLength) {
+  $button[handleIndex].addEventListener('click', (e) => {
+    clickHandler(e);
+  });
+  handleIndex++;
+};
