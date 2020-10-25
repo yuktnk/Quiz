9f3d1985bdc77b7b2e985aa68e95625d7a58f349
yuktnk <yuki0wl@gmail.com>
2020-10-25 18:30:33 +0900

    クイズを３問に

--- app.js
+++ app.js
@@ -1,29 +1,65 @@
-const question = 'ここに問題';
-const ansewers = [
-  '不正解',
-  '正解',
-  '不正解',
-  '不正解'
-];
-const correct = '正解';
+const quizzes = [
+  {
+    question: '問題',
+    answers: [
+      '不正解',
+      '正解',
+      '不正解',
+      '不正解'
+    ],
+    correct: '正解'
+  },
+  {
+    question: '問題2',
+    answers: [
+      '不正解',
+      '正解',
+      '不正解',
+      '不正解'
+    ],
+    correct: '正解'
+  },
+  {
+    question: '問題3',
+    answers: [
+      '不正解',
+      '正解',
+      '不正解',
+      '不正解'
+    ],
+    correct: '正解'
+  },
+]
+
+const quizLength = quizzes.length;
+let quizIndex = 0;
+
+
+
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 
 const setupQuiz = () => {
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quizzes[quizIndex].question;
   let buttonIndex = 0;
   while (buttonIndex < buttonLength) {
-    $button[buttonIndex].textContent = ansewers[buttonIndex];
+    $button[buttonIndex].textContent = quizzes[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 }
 setupQuiz();
 
 const clickHandler = (e) => {
-  if (correct === e.target.textContent) {
+  if (quizzes[quizIndex].correct === e.target.textContent) {
     window.alert('正解！');
   } else {
     window.alert('不正解！');
+  }
+  quizIndex++;
+  if (quizIndex < quizLength) {
+    setupQuiz();
+  } else {
+    window.alert('終了！');
   }
 }
 
